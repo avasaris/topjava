e8dbf75093abc2b552abd111512abd83eb7bf60b
avasaris <artvas@yandex.ru>
2021-02-22 12:47:31 +0200

    HW03 (1-2 tasks)

--- src/main/resources/db/populateDB.sql
+++ src/main/resources/db/populateDB.sql
@@ -1,4 +1,5 @@
 DELETE FROM user_roles;
+DELETE FROM meals;
 DELETE FROM users;
 ALTER SEQUENCE global_seq RESTART WITH 100000;
 
@@ -9,3 +10,22 @@ VALUES ('User', 'user@yandex.ru', 'password'),
 INSERT INTO user_roles (role, user_id)
 VALUES ('USER', 100000),
        ('ADMIN', 100001);
+
+INSERT INTO meals (datetime, description, calories, user_id)
+VALUES  ('2021-02-01 10:00', 'Завтрак', 500,  100000),
+        ('2021-02-01 13:00', 'Обед', 1000,  100000),
+        ('2021-02-01 20:00', 'Ужин', 500,  100000),
+        ('2021-02-02 00:00', 'Еда на граничное значение', 100,  100001),
+        ('2021-02-02 10:00', 'Завтрак', 1000,  100001),
+        ('2021-02-02 13:00', 'Обед', 500,  100001),
+        ('2021-02-02 20:00', 'Ужин', 410,  100001);
+
+
+
+-- new Meal(LocalDateTime.of(2020, Month.JANUARY, 30, 10, 0), "Завтрак", 500),
+-- new Meal(LocalDateTime.of(2020, Month.JANUARY, 30, 13, 0), "Обед", 1000),
+-- new Meal(LocalDateTime.of(2020, Month.JANUARY, 30, 20, 0), "Ужин", 500),
+-- new Meal(LocalDateTime.of(2020, Month.JANUARY, 31, 0, 0), "Еда на граничное значение", 100),
+-- new Meal(LocalDateTime.of(2020, Month.JANUARY, 31, 10, 0), "Завтрак", 1000),
+-- new Meal(LocalDateTime.of(2020, Month.JANUARY, 31, 13, 0), "Обед", 500),
+-- new Meal(LocalDateTime.of(2020, Month.JANUARY, 31, 20, 0), "Ужин", 410)
